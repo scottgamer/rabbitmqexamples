86766f8b4720fa50ba184849bcd0723e21b143f0
rmunoztrade <richard.munoz@trade.ec>
2019-01-28 11:41:48 -0500

    Change to arrow functions

--- 2.Work Queues/new_task.js
+++ 2.Work Queues/new_task.js
@@ -1,12 +1,14 @@
 const amqp = require('amqplib/callback_api');
 
+/**Connect to rabbitmq server */
 amqp.connect('amqp://localhost', (err, conn) => {
     if (err) throw err;
     conn.createChannel((err, ch) => {
         if (err) throw err;
+        /**Instantiate queue */
         const q = 'task_queue';
         let msg = process.argv.slice(2).join(' ') || "Hello World!";
-
+        /**Make queue persistant */
         ch.assertQueue(q, { durable: true });
         ch.sendToQueue(q, new Buffer.from(msg), { persistent: true });
         console.log(" [x] Sent '%s'", msg);
